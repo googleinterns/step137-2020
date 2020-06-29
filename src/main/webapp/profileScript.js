8ecc68046081eaed328be94e86e7aa4c43c8e05d
Vivian Ellis <vivian.c.ellis@gmail.com>
2020-06-29 23:31:22 +0000

    Add functions to make login display code more readable.

--- src/main/webapp/profileScript.js
+++ src/main/webapp/profileScript.js
@@ -1,37 +1,49 @@
 /*
- * Loads certain buttons in the navbar based on the user's login status.
+ * Updates the page's display based on the user's login status.
  */
-function getLoginStatus() {
+function loginStatusDisplay() {
+  // Clear the user-specific elements previously displayed.
   const userNavbarSection = document.getElementById('user-navbar-section');
   userNavbarSection.innerHTML = '';
 
+  // Get the login status and display the corresponding elements.
   const promise = fetch('/login').then(response => response.json()).then((json) => {
-
-    // If the user is logged in, add a logout and profile button.
     if (json['loginStatus'].localeCompare('true') == 0) {
-      const logoutButton = document.createElement('button');
-      logoutButton.innerText = 'Logout';
-      logoutButton.addEventListener('click', () => {
-        window.location.href = json['logoutUrl'];
-      });
-
-      const personalProfileButton = document.createElement('button');
-      personalProfileButton.innerText = 'My Profile';
-      personalProfileButton.addEventListener('click', () => {
-        window.location.href = 'profile.html';
-      });
-
-      userNavbarSection.appendChild(logoutButton);
-      userNavbarSection.appendChild(personalProfileButton);
-
-    // If the user is logged out, add a login button.
+      addLoggedInDisplay();
     } else {
-      const loginButton = document.createElement('button');
-      loginButton.innerText = 'Login';
-      loginButton.addEventListener('click', () => {
-        window.location.href = json['loginUrl'];
-      });
-      userNavbarSection.appendChild(loginButton);
+      addLoggedOutDisplay();
     }
   });
+}
+
+/*
+ * Adds the elements that should be displayed when the user is logged in.
+ */
+function addLoggedInDisplay() {
+  // Add the logout button and profile page button to the navbar.
+  const logoutButton = document.createElement('button');
+  logoutButton.innerText = 'Logout';
+  logoutButton.addEventListener('click', () => {
+    window.location.href = json['logoutUrl'];
+  });
+  const personalProfileButton = document.createElement('button');
+  personalProfileButton.innerText = 'My Profile';
+  personalProfileButton.addEventListener('click', () => {
+    window.location.href = 'profile.html';
+  });
+  userNavbarSection.appendChild(logoutButton);
+  userNavbarSection.appendChild(personalProfileButton);
+}
+
+/*
+ * Adds the elements that should be displayed when the user is logged out.
+ */
+function addLoggedOutDisplay() {
+  // Add the login button to the navbar.
+  const loginButton = document.createElement('button');
+  loginButton.innerText = 'Login';
+  loginButton.addEventListener('click', () => {
+    window.location.href = json['loginUrl'];
+  });
+  userNavbarSection.appendChild(loginButton);
 }
