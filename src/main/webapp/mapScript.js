cf24bee30771b150e81def3b1cecac4c68a4d118
Omarzintan <zintanomar@gmail.com>
2020-06-30 21:16:27 +0000

    add button for creating an event

--- src/main/webapp/mapScript.js
+++ src/main/webapp/mapScript.js
@@ -52,29 +52,34 @@ function fetchPlaceInformation( place_id ) {
   var fetchUrl = 'https://maps.googleapis.com/maps/';
   fetchUrl += 'api/place/details/json?place_id='+ place_id;
   fetchUrl += '&fields=name,rating,formatted_address,website,business_status';
-  fetchUrl += '&key='+API_KEY;
+  fetchUrl += '&key=' + API_KEY;
   fetch(proxyUrl + fetchUrl)
   .then(response => response.json())
   .then(result => { 
     console.log(result.result);
     sideBarElement = document.getElementById('side');
-    sideBarElement.innerHTML = '';
-    infoDivElement = document.createElement('div');
+    infoDivElement = document.getElementById('place-info');
+    infoDivElement.innerHTML = '';
     nameElement = document.createElement('p');
     ratingElement = document.createElement('p');
     addressElement = document.createElement('p');
     websiteElement = document.createElement('a');
+    createEventElement = document.createElement('a');
     businessStatusElement = document.createElement('p');    
     nameElement.innerText = 'Name: ' + result.result.name;
     ratingElement.innerText = 'Rating: ' + result.result.rating;
     addressElement.innerText = 'Address: ' + result.result.formatted_address;
+    websiteElement.innerText = result.result.website;
     websiteElement.href = result.result.website;
+    createEventElement.innerText = 'Create an Event';
+    createEventElement.href = 'CreateAnEvent.html?location=' + result.result.name;
     businessStatusElement.innerText = 'Business Status: ' + result.result.business_status;
     infoDivElement.appendChild(nameElement);
     infoDivElement.appendChild(ratingElement);
     infoDivElement.appendChild(addressElement);
     infoDivElement.appendChild(websiteElement);
     infoDivElement.appendChild(businessStatusElement);
+    infoDivElement.appendChild(createEventElement);
     sideBarElement.appendChild(infoDivElement);
     return sideBarElement;
   })
