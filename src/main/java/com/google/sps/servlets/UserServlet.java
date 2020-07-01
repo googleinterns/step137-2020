2845d4aff0cab39e4854607483d20a57295dd050
Vivian Ellis <vivian.c.ellis@gmail.com>
2020-07-01 01:40:55 +0000

    Add option for single user to be fetched by ID.

--- src/main/java/com/google/sps/servlets/UserServlet.java
+++ src/main/java/com/google/sps/servlets/UserServlet.java
@@ -17,24 +17,43 @@ public class UserServlet extends HttpServlet {
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    Query query = new Query(Constants.USER_ENTITY_PARAM);
-
     DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
-    PreparedQuery results = datastore.prepare(query);
-
     List<User> users = new ArrayList<>();
 
-    for (Entity userEntity : results.asIterable()) {
-      String id = (String) userEntity.getProperty(Constants.USER_ID_PARAM);
-      String name = (String) userEntity.getProperty(Constants.USER_NAME_PARAM);
-
-      User user = new User(id, name);
-      users.add(user);
+    String idParam = request.getParameter(Constants.USER_ID_PARAM);
+    if (idParam != null) {
+      getUserById(datastore, users, idParam);
+    } else {
+      getAllUsers(datastore, users);
     }
 
     Gson gson = new Gson();
 
     response.setContentType("application/json");
     response.getWriter().println(gson.toJson(users));
+  }
+  
+  /** Adds the user with the specified ID from datastore to the list of users. */
+  private void getUserById(DatastoreService datastore, List<User> users, String userId) {
+    Query query = new Query(Constants.USER_ENTITY_PARAM)
+        .setFilter(new Query.FilterPredicate(Constants.USER_ID_PARAM, Query.FilterOperator.EQUAL, userId));
+    PreparedQuery results = datastore.prepare(query);
+    Entity userEntity = results.asSingleEntity();
+    String id = (String) userEntity.getProperty(Constants.USER_ID_PARAM);
+    String name = (String) userEntity.getProperty(Constants.USER_NAME_PARAM);
+    User user = new User(id, name);
+    users.add(user);
+  }
+
+  /** Adds all the users from datastore to the list of users. */
+  private void getAllUsers(DatastoreService datastore, List<User> users) {
+    Query query = new Query(Constants.USER_ENTITY_PARAM);
+    PreparedQuery results = datastore.prepare(query);
+    for (Entity userEntity : results.asIterable()) {
+      String id = (String) userEntity.getProperty(Constants.USER_ID_PARAM);
+      String name = (String) userEntity.getProperty(Constants.USER_NAME_PARAM);
+      User user = new User(id, name);
+      users.add(user);
+    }
   }
 }
