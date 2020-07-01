9e9a6b3f546404d3fd91f71127a6a4d87409c111
Vivian Ellis <vivian.c.ellis@gmail.com>
2020-07-01 00:54:31 +0000

    Add profile servlet to help with profile loading.

--- src/main/webapp/searchUsersScript.js
+++ src/main/webapp/searchUsersScript.js
@@ -18,6 +18,21 @@ function displayUsers() {
 function loadUser(user) {
   const userDisplay = document.createElement('p');
   userDisplay.innerText = user.name;
-  userDisplay.setAttribute('data-id', user.id);
+  userDisplay.addEventListener('click', () => {
+    visitProfile(user.id);
+  });
   return userDisplay;
+}
+
+/*
+ * Redirects the site to the clicked user's profile.
+ */
+function visitProfile(userId) {
+  const params = new URLSearchParams();
+  params.append('id', userId);
+  const request = new Request('/profile', {method: 'POST', body: params});
+  const promise = fetch(request);
+  promise.then(
+    window.location.href = 'profile.html'
+  );
 }
