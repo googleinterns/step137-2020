47600d6789d1dd831c1462036421297f8526b031
Vivian Ellis <vivian.c.ellis@gmail.com>
2020-07-07 03:59:00 +0000

    Fix user name servlet indentation.

--- src/main/java/com/google/sps/servlets/UserNameServlet.java
+++ src/main/java/com/google/sps/servlets/UserNameServlet.java
@@ -19,59 +19,59 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/user-name")
 public class UserNameServlet extends HttpServlet {
 
-    @Override
-    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-      UserService userService = UserServiceFactory.getUserService();
-      DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+  @Override
+  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    UserService userService = UserServiceFactory.getUserService();
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
 
-      String name = request.getParameter(Constants.USER_NAME_PARAM);
-      String id = userService.getCurrentUser().getUserId();
+    String name = request.getParameter(Constants.USER_NAME_PARAM);
+    String id = userService.getCurrentUser().getUserId();
 
-      Entity existingUserEntity = getUserEntity(id);
+    Entity existingUserEntity = getUserEntity(id);
 
-      // If a User entity with the current user's id does not already exist, create it.
-      if (existingUserEntity == null) {
-        Entity newUserEntity = createNewUser(id, name);
-        datastore.put(newUserEntity);
+    // If a User entity with the current user's id does not already exist, create it.
+    if (existingUserEntity == null) {
+      Entity newUserEntity = createNewUser(id, name);
+      datastore.put(newUserEntity);
       
-      // If the User entity exists, put it in datastore with its updated "name" property.
-      } else {
-        existingUserEntity.setProperty(Constants.USER_NAME_PARAM, name);
-        datastore.put(existingUserEntity);
-      }
+    // If the User entity exists, put it in datastore with its updated "name" property.
+    } else {
+      existingUserEntity.setProperty(Constants.USER_NAME_PARAM, name);
+      datastore.put(existingUserEntity);
+    }
     
-      response.sendRedirect("/profile.html");
+    response.sendRedirect("/profile.html");
+  }
+
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    UserService userService = UserServiceFactory.getUserService();
+
+    // Get the name property of the current User entity, or an empty string otherwise. 
+    String name = "";
+    Entity currentUserEntity = getUserEntity(userService.getCurrentUser().getUserId());
+    if (currentUserEntity != null) {
+      name = (String) currentUserEntity.getProperty(Constants.USER_NAME_PARAM);
     }
 
-    @Override
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-      UserService userService = UserServiceFactory.getUserService();
+    response.setContentType("text/html");
+    response.getWriter().println(name);
+  }
 
-      // Get the name property of the current User entity, or an empty string otherwise. 
-      String name = "";
-      Entity currentUserEntity = getUserEntity(userService.getCurrentUser().getUserId());
-      if (currentUserEntity != null) {
-        name = (String) currentUserEntity.getProperty(Constants.USER_NAME_PARAM);
-      }
+  /** Returns the User entity with the specified "id" property, or null if one could not be found. */
+  private Entity getUserEntity(String id) {
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    Query query = new Query(Constants.USER_ENTITY_PARAM)
+        .setFilter(new Query.FilterPredicate(Constants.USER_ID_PARAM, Query.FilterOperator.EQUAL, id));
+    PreparedQuery results = datastore.prepare(query);
+    return results.asSingleEntity();
+  }
 
-      response.setContentType("text/html");
-      response.getWriter().println(name);
-    }
-
-    /** Returns the User entity with the specified "id" property, or null if one could not be found. */
-    private Entity getUserEntity(String id) {
-      DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
-      Query query = new Query(Constants.USER_ENTITY_PARAM)
-          .setFilter(new Query.FilterPredicate(Constants.USER_ID_PARAM, Query.FilterOperator.EQUAL, id));
-      PreparedQuery results = datastore.prepare(query);
-      return results.asSingleEntity();
-    }
-
-    /** Returns a newly created User entity with the specified ID and name. */
-    private Entity createNewUser(String id, String name) {
-      Entity newUserEntity = new Entity(Constants.USER_ENTITY_PARAM);
-      newUserEntity.setProperty(Constants.USER_ID_PARAM, id);
-      newUserEntity.setProperty(Constants.USER_NAME_PARAM, name);
-      return newUserEntity;
-    }
+  /** Returns a newly created User entity with the specified ID and name. */
+  private Entity createNewUser(String id, String name) {
+    Entity newUserEntity = new Entity(Constants.USER_ENTITY_PARAM);
+    newUserEntity.setProperty(Constants.USER_ID_PARAM, id);
+    newUserEntity.setProperty(Constants.USER_NAME_PARAM, name);
+    return newUserEntity;
+  }
 }
