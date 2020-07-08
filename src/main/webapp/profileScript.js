cba84cee0004030ae26b8600daecf24d0f630c68
Vivian Ellis <vivian.c.ellis@gmail.com>
2020-07-08 04:10:33 +0000

    Display user names in buddies list (that link to their profiles).

--- src/main/webapp/profileScript.js
+++ src/main/webapp/profileScript.js
@@ -196,17 +196,33 @@ function additionalDisplay(user) {
  * Displays information and options for if the user is viewing their own profile.
  */
 function personalDisplay() {
-  // Add an option for the user to change their display name.
+  // Add an option for the current user to change their display name.
   const changeNameButton = document.createElement('button');
   changeNameButton.innerText = 'Change name';
   changeNameButton.addEventListener('click', () => {
     showNameForm();
   });
 
-  // Add the list of the user's buddies.
-  buddiesList = document.createElement('p');
+  // Add the list of the current user's buddies.
+  const buddiesList = document.createElement('div');
+  const buddiesHeading = document.createElement('h3');
+  buddiesHeading.innerText = 'Your buddies:';
+  buddiesList.appendChild(buddiesHeading);
   fetch('/buddy').then(response => response.json()).then((buddies) => {
-    buddiesList.innerText = 'Your buddies: ' + buddies;
+    fetch('/user').then(response => response.json()).then((users) => {
+      for (let i = 0; i < users.length; i ++) {
+        if (buddies.includes(users[i].id)) {
+          // If the user's ID is in the list of the current user's buddies,
+          // add their name (which links to their profile) to the page. 
+          const buddyElement = document.createElement('p');
+          buddyElement.innerText = users[i].name;
+          buddyElement.addEventListener('click', () => {
+            visitProfile(users[i].id);
+          });
+          buddiesList.appendChild(buddyElement);
+        }
+      }
+    });
   });
 
   const basicInfoContainer = document.getElementById('basic-info');
