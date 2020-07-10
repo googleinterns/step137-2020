473a992dd4f69414ec366e706c0eae53f7b7a491
Omarzintan <zintanomar@gmail.com>
2020-07-10 21:10:50 +0000

    modify openTab function to ensure tabs display appropriately

--- src/main/webapp/mapScript.js
+++ src/main/webapp/mapScript.js
@@ -203,10 +203,12 @@ function openTab(evt, tabName) {
   }
   tablinks = document.getElementsByClassName('tablinks');
   for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace("active", "");
+    tablinks[i].className = tablinks[i].className.replace('active', '');
+    tablins[i].id = tablinks[i].id.replace('defaultOpen', '');
   }
   document.getElementById(tabName).style.display = 'block';
-  evt.currentTarget.className += "active";
+  evt.currentTarget.className += 'active';
+  evt.currentTarget.id += 'defaultOpen';
 
   document.getElementById('defaultOpen').click();
 }
