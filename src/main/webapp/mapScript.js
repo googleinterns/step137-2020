e8067c0bbace02b67d0e53b351ce1e76914713d9
Lexi Delorey <60301386+lexidelorey@users.noreply.github.com>
2020-07-15 14:36:50 -0700

    fixing event display (again lol)

--- src/main/webapp/mapScript.js
+++ src/main/webapp/mapScript.js
@@ -360,8 +360,17 @@ function getAvailableEvents(userID) {
     .then(events => {
       for (i = 0; i < events.length; i++) {
         if (events[i].location == locationName) {
+          invitedAttendees = events[i].invitedAttendees;
           rsvpAttendees = events[i].rsvpAttendees;
-          if (rsvpAttendees.includes(userID)) {
+          rsvpContains = rsvpAttendees.includes(userID);
+          //if event is public, user should be able to see it regardless of whether they plan 
+          //to attend
+          if (events[i].privacy == "public") {
+            if (!rsvpContains) {
+              eventDivElement.appendChild(createEventAttendees(events[i], userID, "false"));
+            }
+          }
+          else if (rsvpContains) {
             eventDivElement.appendChild(createEventAttendees(events[i], userID, "true"));
           }
           else if (invitedAttendees.includes(userID)) {
