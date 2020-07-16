1b0ae479e7eafa419c33fa8942b3841e818e6d1a
Vivian Ellis <vivian.c.ellis@gmail.com>
2020-07-16 21:44:34 +0000

    Update user storage and access to include buddy requests.

--- src/main/java/com/google/sps/data/User.java
+++ src/main/java/com/google/sps/data/User.java
@@ -7,10 +7,12 @@ public final class User {
   private final String id;
   private final String name;
   private final List<String> buddies;
+  private final List<String> buddyRequests;
 
-  public User(String id, String name, List<String> buddies) {
+  public User(String id, String name, List<String> buddies, List<String> buddyRequests) {
     this.id = id;
     this.name = name;
     this.buddies = buddies;
+    this.buddyRequests = buddyRequests;
   }
 }
