91a473d58fdc2507e74970cc6341e2add1a34eca
Vivian Ellis <vivian.c.ellis@gmail.com>
2020-07-23 02:27:09 +0000

    Display option to upload profile photo on hover.

--- src/main/webapp/profileScript.js
+++ src/main/webapp/profileScript.js
@@ -158,6 +158,10 @@ function displayBasicInfo(user, viewer) {
   const profilePicContainer = document.getElementById('profile-pic-container');
   displayProfilePicture(user, profilePicContainer, 'profile-pic-large');
 
+  const editImageButton = document.createElement('i');
+  editImageButton.id = 'edit-image-button';
+  editImageButton.className = 'fa fa-edit';
+
   const nameContainer = document.getElementById('name-container');
   nameContainer.innerHTML = '';
 
@@ -169,20 +173,21 @@ function displayBasicInfo(user, viewer) {
   editNameButton.className = 'fa fa-edit';
 
   if (viewer === PROFILE_VIEWER_PERSONAL) {
-    // Add an option for the current user to change their display name.
+    // On hover, display an option for the current user to change their name.
     name.id = 'personal-name';
     nameContainer.addEventListener('click', () => {
       showNameForm();
     });
-
-    // Add an option for the current user to change their profile picture.
-    // const editImageButton = document.createElement('button');
-    // editImageButton.innerText = 'Change profile picture';
-    // editImageButton.addEventListener('click', () => {
-    //   showImageForm();
-    // });
-    // nameContainer.append(editImageButton);
+    
+    // On hover, display an option for the current user to change their profile picture.
+    profilePicContainer.id = 'personal-pic-container'
+    profilePicContainer.addEventListener('click', () => {
+      showImageForm();
+    });
+    const personalPic = profilePicContainer.childNodes[0];
+    personalPic.id = 'personal-pic';
   }
+  profilePicContainer.appendChild(editImageButton);
   nameContainer.appendChild(name);
   nameContainer.appendChild(editNameButton);
 }
