f3cc5d2c262adaf560e0b31c38256747b1be48b3
Vivian Ellis <vivian.c.ellis@gmail.com>
2020-07-23 03:16:45 +0000

    Add smaller right-aligned request approve/remove buttons.

--- src/main/webapp/profileScript.js
+++ src/main/webapp/profileScript.js
@@ -339,13 +339,13 @@ function displayBuddyRequests(user) {
           const requestButtons = document.createElement('div');
           requestButtons.className = 'request-buttons';
           const approveButton = document.createElement('button');
-          approveButton.className = 'button';
+          approveButton.className = 'request-button';
           approveButton.innerText = 'Approve';
           approveButton.addEventListener('click', () => {
             addOrRemoveBuddy(users[i], 'add');
           });
           const removeButton = document.createElement('button');
-          removeButton.className = 'button';
+          removeButton.className = 'request-button';
           removeButton.innerText = 'Remove';
           removeButton.addEventListener('click', () => {
             sendOrRemoveBuddyRequest(users[i], 'remove');
