bcb9924bb6354307bd6b2b0bbb3e7dc2cc14c245
Vivian Ellis <vivian.c.ellis@gmail.com>
2020-07-27 21:03:57 +0000

    Display searched users in order with those that contain the search text earlier first.

--- src/main/webapp/searchUsersScript.js
+++ src/main/webapp/searchUsersScript.js
@@ -6,31 +6,55 @@ function searchUsersOnload() {
 }
 
 /*
- * Displays the found users from the current user's search.
+ * Finds the users whose names contain the searched text.
  */
-function displaySearchedUsers() {
-  const searchedUsers = document.getElementById('searched-users');
-  searchedUsers.style = 'height: 100%';
+function searchUsers() {
   const searchText = document.getElementById('search-text').value;
-  if (searchText === '') {
-    // Don't return results for an empty search.
-    searchedUsers.innerHTML = '';
-  } else {
+  if (searchText !== '') {
+  let searchedUsers = [];
     fetch('/user').then(response => response.json()).then((users) => {
-      searchedUsers.innerHTML = '';
-      let userCount = 0;
       for (let i = 0; i < users.length; i ++) {
-        if (users[i].name.toLowerCase().includes(searchText.toLowerCase())) {
-          // Display any users whose name contains the search text.
-          searchedUsers.appendChild(createUserElement(users[i]));
-          userCount ++;
+        let searchIndex = users[i].name.toLowerCase().indexOf(searchText.toLowerCase());
+        if (searchIndex != -1) {
+          searchedUsers.push([users[i], searchIndex]);
         }
       }
-      if (userCount == 0) {
-        const searchMessage = document.createElement('p');
-        searchMessage.innerText = 'No users with that name could be found.';
-        searchedUsers.appendChild(searchMessage);
-      }
+      displaySearchedUsers(searchedUsers);
     });
+  } else {
+    displaySearchedUsers([]);
+  }
+}
+
+/*
+ * Displays the searched users in order of those with the search text
+ * contained earlier in their names first.
+ */
+function displaySearchedUsers(searchedUsers) {
+  const searchedUsersContainer = document.getElementById('searched-users');
+  searchedUsersContainer.innerHTML = '';
+  searchedUsersContainer.style = 'height: 100%';
+  if (searchedUsers.length == 0) {
+    // Display a message if no users' names contained the search text.
+    const searchMessage = document.createElement('p');
+    searchMessage.innerText = 'No users with that name could be found.';
+    searchedUsersContainer.appendChild(searchMessage);
+  } else {
+    let userIndex = 0;
+    let firstUser = searchedUsers[userIndex];
+    while (searchedUsers.length > 0) {
+      for (let i = 0; i < searchedUsers.length; i ++) {
+        if (searchedUsers[i][1] < firstUser[1]) {
+          userIndex = i;
+          firstUser = searchedUsers[userIndex];
+        }
+      }
+      // Display the user with the earliest index of the search text.
+      searchedUsersContainer.appendChild(createUserElement(firstUser[0]));
+      // Remove that user from the list and set a new placeholder first user.
+      searchedUsers.splice(userIndex, 1);
+      userIndex = 0;
+      firstUser = searchedUsers[userIndex];
+    }
   }
 }
