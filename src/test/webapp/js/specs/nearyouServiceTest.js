f08c1e08ce410af76423cce32ec5b614630496e3
Omarzintan <zintanomar@gmail.com>
2020-08-05 22:26:17 +0000

    add test to only display nearby events

--- src/test/webapp/js/specs/nearyouServiceTest.js
+++ src/test/webapp/js/specs/nearyouServiceTest.js
@@ -4,6 +4,7 @@ describe('the nearyou service', function() {
   beforeEach(function() {
     nearyouService = new nearyouScript();
     events = [];
+    eventObjs = [];
     currLocation = new Object();
     currLocation.xlocation = 0;
     currLocation.ylocation = 0;
@@ -20,16 +21,35 @@ describe('the nearyou service', function() {
     eventObj.xlocation = 1500;
     eventObj.ylocation = 0;
     var isNearby = nearyouService.isNearby(eventObj, currLocation);
-    expect(isNearby).toBe('true');
+    expect(isNearby).toBe(true);
   });
 
   it('must detect when an event is not nearby', function() {
-    eventObj1 = new Object();
-    eventObj1.event = 'Event 1';
-    eventObj1.xlocation = 2200;
-    eventObj1.ylocation = 0;
-    var isNearby = nearyouService.isNearby(eventObj1, currLocation);
-    expect(isNearby).toBe('false');
+    eventObj = new Object();
+    eventObj.event = 'Event 1';
+    eventObj.xlocation = 2200;
+    eventObj.ylocation = 0;
+    var isNearby = nearyouService.isNearby(eventObj, currLocation);
+    expect(isNearby).toBe(false);
   });
   
+  it('must only return nearby events', function() {
+    eventObj1 = new Object();
+    eventObj1.event = 'Far event';
+    eventObj1.xlocation = 2200;
+    eventObj1.ylocation = 0;
+    eventObjs.push(eventObj1);
+    eventObj2 = new Object();
+    eventObj2.event = 'Near event';
+    eventObj2.xlocation = 1500;
+    eventObj2.ylocation = 500;
+    eventObjs.push(eventObj2);
+    var nearbyEvents = nearyouService.findNearbyEvents(eventObjs, currLocation);
+    expect(nearbyEvents).toEqual([eventObj2]);
+  });
+
+  it('must display events in order of the nearest events',function() {
+    //create event objects with varying distances to currLocation
+    //expect the resulting array to be in order of nearness to you current Location
+  })
 });
