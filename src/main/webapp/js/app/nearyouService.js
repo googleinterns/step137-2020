7a542c5ea14d93791e7bd4e4fe9deae1e0610de3
Omarzintan <zintanomar@gmail.com>
2020-08-07 05:39:03 +0000

    implement changes suggested and add addtional tests

--- src/main/webapp/js/app/nearyouService.js
+++ src/main/webapp/js/app/nearyouService.js
@@ -3,21 +3,51 @@ function nearyouScript() {
 nearyouScript.prototype.locationRadius = 2000;
 nearyouScript.prototype.events = [];
 nearyouScript.prototype.eventObjs = [];
-nearyouScript.prototype.findNearbyEvents = function(events, currLocation) {
-  'use strict';
-  if (events.length == 0) {
+nearyouScript.prototype.nearyouEvents = [];
+nearyouScript.prototype.findNearbyEvents = function(eventObjList, currLocation) {
+  if (eventObjList.length == 0) {
     return "No nearby events";
+  }
+  else {
+    this.nearyouEvents = [];
+    for (var i = 0; i < eventObjList.length; i++) {
+      if (this.isNearby(eventObjList[i], currLocation)) {
+        var distanceFromLocation = this.getDistance(eventObjList[i], currLocation);
+        eventObjList[i].distance = distanceFromLocation;
+        this.nearyouEvents.push(eventObjList[i]);
+      }
+    }
+    // return this.nearyouEvents.length;
+    this.nearyouEvents.sort( this.compareDistancetoCurrLocation );
+    return this.nearyouEvents;
   }
 };
 
 nearyouScript.prototype.isNearby = function(eventObj, currLocation) {
-  
   var distanceFromCurrLocation = Math.sqrt(
     (Math.pow((eventObj.xlocation - currLocation.xlocation), 2)) + 
     (Math.pow((eventObj.ylocation - currLocation.ylocation), 2)) 
   );
   if (distanceFromCurrLocation <= 2000) {
-    return 'true';
+    return true;
   }
-  else { return 'false'; }
+  else { return false; }
 };
+
+nearyouScript.prototype.getDistance = function(eventObj, currLocation) {
+  var distance = Math.sqrt(
+    (Math.pow((eventObj.xlocation - currLocation.xlocation), 2)) + 
+    (Math.pow((eventObj.ylocation - currLocation.ylocation), 2)) 
+  );
+  return distance;
+}
+
+nearyouScript.prototype.compareDistancetoCurrLocation = function (eventObj1, eventObj2) {
+  if (eventObj1.distance < eventObj2.distance) {
+    return -1;
+  }
+  if (eventObj1.distance > eventObj2.distance) {
+    return 1;
+  }
+  return 0;
+}
--- src/test/webapp/js/specs/nearyouServiceTest.js
+++ src/test/webapp/js/specs/nearyouServiceTest.js
@@ -5,51 +5,50 @@ describe('the nearyou service', function() {
     nearyouService = new nearyouScript();
     events = [];
     eventObjs = [];
-    currLocation = new Object();
-    currLocation.xlocation = 0;
-    currLocation.ylocation = 0;
+    currLocation = {xlocation : 0, ylocation : 0};
   });
 
-  it('must detect when there are no events', function () {
+  it('detects when there are no events', function () {
     var response = nearyouService.findNearbyEvents(events, currLocation);
     expect(response).toBe('No nearby events');
   });
 
-  it('must detect whether an event is nearby', function() {
-    eventObj = new Object();
-    eventObj.event = 'Event 1';
-    eventObj.xlocation = 1500;
-    eventObj.ylocation = 0;
+  it('detects whether an event is nearby', function() {
+    eventObj = {event : 'Event 1', xlocation : 1500, ylocation : 0};
     var isNearby = nearyouService.isNearby(eventObj, currLocation);
     expect(isNearby).toBe(true);
   });
 
-  it('must detect when an event is not nearby', function() {
-    eventObj = new Object();
-    eventObj.event = 'Event 1';
-    eventObj.xlocation = 2200;
-    eventObj.ylocation = 0;
+  it('detects when an event is not nearby', function() {
+    eventObj = {event : 'Event 1', xlocation : 2200, ylocation : 0};
     var isNearby = nearyouService.isNearby(eventObj, currLocation);
     expect(isNearby).toBe(false);
   });
   
-  it('must only return nearby events', function() {
-    eventObj1 = new Object();
-    eventObj1.event = 'Far event';
-    eventObj1.xlocation = 2200;
-    eventObj1.ylocation = 0;
+  it('returns only nearby events', function() {
+    eventObj1 = {event : 'Far event', xlocation : 2200, ylocation : 0};
     eventObjs.push(eventObj1);
-    eventObj2 = new Object();
-    eventObj2.event = 'Near event';
-    eventObj2.xlocation = 1500;
-    eventObj2.ylocation = 500;
+    eventObj2 = {event : 'Near event', xlocation : 1500, ylocation : 500};
     eventObjs.push(eventObj2);
     var nearbyEvents = nearyouService.findNearbyEvents(eventObjs, currLocation);
     expect(nearbyEvents).toEqual([eventObj2]);
   });
 
-  it('must display events in order of the nearest events',function() {
-    //create event objects with varying distances to currLocation
-    //expect the resulting array to be in order of nearness to you current Location
-  })
+  it('displays events in order of the nearest events', function() {
+    eventObjs = [
+      {event : 'Farthest event', xlocation : 1900, ylocation : 0},
+      {event : 'Second Nearest event', xlocation : 1500, ylocation : 0},
+      {event : 'Nearest event', xlocation : 1200, ylocation : 0}
+    ];
+    var nearbyEventsInOrder = nearyouService.findNearbyEvents(eventObjs, 
+      currLocation
+    );
+    expect(nearbyEventsInOrder).toEqual(
+      [
+        {event : 'Nearest event', xlocation : 1200, ylocation : 0, distance : 1200},
+        {event : 'Second Nearest event', xlocation : 1500, ylocation : 0, distance : 1500},
+        {event : 'Farthest event', xlocation : 1900, ylocation : 0, distance : 1900}
+      ]
+    );
+  });
 });
